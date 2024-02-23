c8abff1ae0b24245fbee605c73193e98846fef20
MusaddekAli <musaddekali.dev@gmail.com>
2024-02-23 12:11:26 +0600

    updated asynchandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,5 @@
-// const asyncHandler = (requestHandler) => {
-//   return async (req, res, next) => {
-//    return Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-//   };
-// };
-
 export const asyncHandler = (requestHandler) => {
-  return async (req, res, next) => {
-    try {
-      await requestHandler(req, res, next);
-    } catch (error) {
-      // res.status(error.code || 500).json({
-      //   statusCode: error.code || 500,
-      //   success: false,
-      //   message: error.message
-      // });
-      next(error);
-    }
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
